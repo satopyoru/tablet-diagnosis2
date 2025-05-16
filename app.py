a09e8819973374a3166e061682e4a09f9215827b
satopyoru <satomi0030@gmail.com>
2025-05-16 11:32:18 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from streamlit_extras.switch_page_button import switch_page
+＃from streamlit_extras.switch_page_button import switch_page
 import pandas as pd
 import plotly.express as px
 
